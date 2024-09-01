e35484c27559ffcfc5a9040b4a79870e1cb57a8e
Marin Aglic <mrn.aglic@gmail.com>
2024-09-01 21:52:07 +0200

    Working example with Dataset

--- dags/data_aware_alias/consumer.py
+++ dags/data_aware_alias/consumer.py
@@ -1,32 +1,52 @@
 import pendulum
 from airflow import DAG
+from airflow.models.dataset import DatasetEvent
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 
-from dags.include.datasets.datasets import ALIAS_DATASET
+from dags.include.datasets.datasets import ENRICHED_DATASET
 
 
-def _print(data_interval_start, data_interval_end, **context):
-    print(f"data_interval_start: {data_interval_start}")
-    print(f"data_interval_end: {data_interval_end}")
+def _resolve_event(event: DatasetEvent) -> dict:
+    return {
+        "ds_interval_start": pendulum.DateTime.fromisoformat(
+            event.extra["data_interval_start"]
+        ),
+        "ds_interval_end": pendulum.DateTime.fromisoformat(
+            event.extra["data_interval_end"]
+        ),
+    }
 
-    inlets_events = context["inlets_events"]
-    print(f"inlets_events: {inlets_events}")
+
+def _resolve_dataset_events(inlet_events: list[DatasetEvent], **context) -> list[dict]:
+
+    return [_resolve_event(event) for event in inlet_events[ENRICHED_DATASET]]
+
+
+def _print(ds_interval_start, ds_interval_end, **context):
+    print(f"data_interval_start: {ds_interval_start}")
+    print(f"data_interval_end: {ds_interval_end}")
 
 
 with DAG(
-    dag_id="simple_consumer_alias",
+    dag_id="simple_consumer_metadata",
     start_date=pendulum.datetime(2023, 10, 15),
-    schedule=[ALIAS_DATASET],
+    schedule=[ENRICHED_DATASET],
 ):
     dummy = EmptyOperator(
         task_id="dummy",
     )
 
-    print_data = PythonOperator(
-        task_id="print_data",
-        python_callable=_print,
-        inlets=[ALIAS_DATASET],
+    resolve_dataset_events = PythonOperator(
+        task_id="resolve_dataset_events",
+        python_callable=_resolve_dataset_events,
+        inlets=[ENRICHED_DATASET],
     )
 
-    dummy >> print_data
+    print_data = PythonOperator.partial(
+        task_id="print_data",
+        python_callable=_print,
+        map_index_template="""resolved_with_start_{{ task.op_kwargs['ds_interval_start'] | ds }}""",
+    ).expand(op_kwargs=resolve_dataset_events.output)
+
+    dummy >> resolve_dataset_events
